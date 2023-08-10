b4579aab1308d646d2cf6d8058243cefe3f5a9c6
CLEMON EZEH <clemonezeh@gmail.com>
2023-08-10 19:36:10 +0100

    email is set

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import nodemailer from "nodemailer";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Email Transporter
@@ -33,22 +35,26 @@ app.get("/", (req, res) => {
 });
 
 app.post("/send", async (req, res) => {
-  const { fullname, email, subject, message } = req.body;
+  try {
+    const { fullname, email, subject, message } = req.body;
 
-  let mailMessage = {
-    from: `<${email}>`, // sender address
-    to: process.env.Email, // list of receivers
-    subject: `${subject}`, // Subject line
-    html: `
+    let mailMessage = {
+      from: `<${email}>`, // sender address
+      to: process.env.Email, // list of receivers
+      subject: `${subject}`, // Subject line
+      html: `
     <body>
-    <b>You have a message from ${fullname}</b>
-    <b> Email: ${email}</b>
+    <b>You have a message from ${fullname},</b>
+    <b>From this Email: ${email}</b>
      <p> ${message}</p>
     </body>`, // html body
-  };
-  const info = await transporter.sendMail(mailMessage);
+    };
+    const info = await transporter.sendMail(mailMessage);
 
-  res.json(info);
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 app.listen(process.env.PORT || 4000, () => {
